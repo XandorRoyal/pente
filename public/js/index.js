8d499a17f07dc6c2f469a1df9dbb4ed992d98df6
Keyana L <klastrap@student.neumont.edu>
2024-02-11 14:24:43 -0700

    just the board

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const boardSize = 19;
     const board = document.getElementById("board");
 
-    // Loop to generate rows
+    // Loop to generate rows and cells within each row
     for (let i = 0; i < boardSize; i++) {
         const row = document.createElement("tr");
 
-        // Loop to generate cells within each row
         for (let j = 0; j < boardSize; j++) {
             const cell = document.createElement("td");
             row.appendChild(cell);
